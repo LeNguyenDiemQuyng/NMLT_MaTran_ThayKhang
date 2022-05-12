217dff7059931b258480fa25b1e271648e04bdca
LeNguyenDiemQuynh <105436103+LeNguyenDiemQuyng@users.noreply.github.com>
2022-05-12 21:31:02 +0700

    Add files via upload

---
+++ Bai049/Bai049.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <string>
+using namespace std;
+
+string getInputFileName(int);
+string getOutputFileName(int);
+void Nhap(int[][500], int&, int&, string);
+void Xuat(int[][500], int, int, string);
+void Xuat(int[][500], int, int);
+int Tong5m(int[][500], int, int);
+bool Kt5m(int);
+
+int main()
+{
+	int m, n;
+	int a[500][500];
+	for (int i = 1; i <= 1; i++)
+	{
+		string FileName = getInputFileName(i);
+		Nhap(a, m, n, FileName);
+
+		cout << "\n" << FileName << endl;
+		Xuat(a, m, n);
+
+		cout << "\n" << "Tong cac so nguyen dang 5^m tren bien la: " << setw(8) << Tong5m(a, m, n);
+
+		FileName = getOutputFileName(i);
+		Xuat(a, m, n, FileName);
+	}
+
+	return 0;
+}
+
+
+string getInputFileName(int n)
+{
+	string fileName = string("intmatrandata");
+	if (n < 10)
+		fileName += "0" + to_string(n) + ".inp";
+	else
+		fileName += to_string(n) + ".inp";
+	return fileName;
+}
+
+string getOutputFileName(int n)
+{
+	string fileName = string("intmatrandata");
+	if (n < 10)
+		fileName += "0" + to_string(n) + ".out";
+	else
+		fileName += to_string(n) + ".out";
+	return fileName;
+}
+
+void Nhap(int a[][500], int& m, int& n, string filename)
+{
+	ifstream fi(filename);
+	fi >> m;
+	fi >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			fi >> a[i][j];
+}
+
+void Xuat(int a[][500], int m, int n, string filename)
+{
+	ofstream fo(filename);
+	fo << setw(8) << m;
+	fo << setw(8) << n;
+	fo << "\n";
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			fo << setw(8) << a[i][j];
+		fo << "\n";
+	}
+}
+
+void Xuat(int a[][500], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << setw(8) << a[i][j];
+		cout << endl;
+	}
+}
+
+bool Kt5m(int n)
+{
+	bool flag = true;
+	if (n < 0)
+		return false;
+	while (n > 1)
+	{
+		int du = n % 5;
+		if (du != 0)
+			flag = false;
+		n = n / 5;
+	}
+	return flag;
+}
+
+int Tong5m(int a[][500], int m, int n)
+{
+	int s = 0;
+	for (int j = 0; j < n - 1; j++)
+		if (Kt5m(a[0][j]) == true)
+			s = s + a[0][j];
+	for (int i = 0; i < m - 1; i++)
+		if (Kt5m(a[i][n - 1]) == true)
+			s = s + a[i][n - 1];
+	for (int j = n - 1; j > 0; j--)
+		if (Kt5m(a[m - 1][j]) == true)
+			s = s + a[m - 1][j];
+	for (int i = m - 1; i > 0; i--)
+		if (Kt5m(a[i][n - 1]) == true)
+			s = s + a[i][n - 1];
+	return s;
+}
